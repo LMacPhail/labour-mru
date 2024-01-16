f079ebce50f425ea50841c60299098dff80ee239
pixelerate <lucymacphail97@gmail.com>
2024-01-16 19:40:20 +0000

    making a start

---
+++ src/Account.tsx
@@ -0,0 +1,102 @@
+import {
+  useState,
+  useEffect,
+  SetStateAction,
+  FormEventHandler,
+  FormEvent,
+} from "react";
+import { supabase } from "./supabaseClient";
+
+export default function Account({ session }: { session: any }) {
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    let ignore = false;
+    async function getProfile() {
+      setLoading(true);
+      const { user } = session;
+
+      const { data, error } = await supabase
+        .from("profiles")
+        .select(`username`)
+        .eq("id", user.id)
+        .single();
+
+      if (!ignore) {
+        if (error) {
+          console.warn(error);
+        } else if (data) {
+          setUsername(data.username);
+        }
+      }
+
+      setLoading(false);
+    }
+
+    getProfile();
+
+    return () => {
+      ignore = true;
+    };
+  }, [session]);
+
+  async function updateProfile(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    setLoading(true);
+    const { user } = session;
+
+    const updates = {
+      id: user.id,
+      username,
+      updated_at: new Date(),
+    };
+
+    const { error } = await supabase.from("profiles").upsert(updates);
+
+    if (error) {
+      alert(error.message);
+    }
+    setLoading(false);
+  }
+
+  return (
+    <form onSubmit={updateProfile} className="form-widget">
+      <div>
+        <label htmlFor="email">Email</label>
+        <input id="email" type="text" value={session.user.email} disabled />
+      </div>
+      <div>
+        <label htmlFor="username">Name</label>
+        <input
+          id="username"
+          type="text"
+          required
+          value={username || ""}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <button
+          className="button block primary"
+          type="submit"
+          disabled={loading}
+        >
+          {loading ? "Loading ..." : "Update"}
+        </button>
+      </div>
+
+      <div>
+        <button
+          className="button block"
+          type="button"
+          onClick={() => supabase.auth.signOut()}
+        >
+          Sign Out
+        </button>
+      </div>
+    </form>
+  );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { Sidebar } from "./components/sidebar/Sidebar";
 import Main from "./components/Main";
@@ -10,6 +10,10 @@ import { fetchMPs } from "./data/utils/utils";
 import { DataStatus, MP } from "./data/types";
 import { SET_DATA_ACTION } from "./state/actions";
 import { useAnalytics } from "./analytics";
+import { supabase } from "./supabaseClient";
+import Auth from "./Auth";
+import Account from "./Account";
+import { Session } from "@supabase/supabase-js";
 
 function App() {
   const view = useSelector((state: AppState) => state.view);
@@ -24,15 +28,32 @@ function App() {
     [dispatch]
   );
   fetchMPs(updateData);
-  useAnalytics()
+  useAnalytics();
+
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
 
   return (
     <div className="">
-      <Header view={view} />
+      {!session ? (
+        <Auth />
+      ) : (
+        <Account key={session.user.id} session={session} />
+      )}
+      {/* <Header view={view} />
       <Sidebar>
         <SidebarContent />
       </Sidebar>
-      <Main view={view} />
+      <Main view={view} /> */}
     </div>
   );
 }
